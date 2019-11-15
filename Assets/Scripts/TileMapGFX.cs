2e4744d15c984c14e74fdbe8a49f655ca69e9d04
Raphael Hungria <rhja@uol.com.br>
2019-11-15 12:30:14 -0300

    first commit/Repo config

---
+++ Assets/Scripts/TileMapGFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class TileMapGFX : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
