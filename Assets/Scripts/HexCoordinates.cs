6c411685c1fe5aa48c863735ec58f0c0aa73032b
Raphael Hungria <rhja@uol.com.br>
2019-11-15 18:35:18 -0300

    Grid/Mesh generation halfway

---
+++ Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates
+{
+    [SerializeField]
+    private int x, z;
+    public int X {get => x;}
+    public int Y { get => -X-Z;}
+    public int Z {get => z;}
+
+    public HexCoordinates (int x, int z){
+        this.x = x;
+        this.z = z;
+    }
+
+    public static HexCoordinates FromOffsetCoords(int x, int z){
+        return new HexCoordinates(x - z/2, z);
+    }
+    public static HexCoordinates FromPosition (Vector3 position){
+        float offset = position.z / (HexMetrics.outerRadius * 3f);
+        float x = position.x / (HexMetrics.innerRadius * 2f);
+        float y = -x;
+        x -= offset;
+        y -= offset;
+
+        int iX = Mathf.RoundToInt(x);
+        int iY = Mathf.RoundToInt(y);
+        int iZ = Mathf.RoundToInt(-x-y);
+
+        if (iX + iY + iZ != 0){
+            float dX = Mathf.Abs(x - iX);
+            float dY = Mathf.Abs(x - iY);
+            float dZ = Mathf.Abs(-x -y - iZ);
+
+            if (dX > dY && dX > dZ){
+                iX = -iY - iZ;
+            }
+            else if (dZ > dY){
+                iZ = -iX - iY;
+            }
+        }
+
+        return new HexCoordinates(iX,iZ);
+    }
+
+    public override string ToString(){
+        return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
+    }
+    public string ToStringOnSeparateLines(){
+        return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
+    }
+}
---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public int xWidht = 6, zHeight = 6;
+    public HexCell cellPrefab;
+    public TMPro.TextMeshProUGUI cellLabelPrefab;
+
+    public Color defaultColor = Color.white, touchedColor = Color.magenta;
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+
+    HexCell[] cells;
+
+    void Awake(){
+        hexMesh = GetComponentInChildren<HexMesh>();
+        gridCanvas = GetComponentInChildren<Canvas>();
+
+        cells = new HexCell[zHeight * xWidht];
+
+        for (int z = 0, i = 0; z < zHeight; z++){
+            for (int x = 0; x < xWidht; x++){
+                CreateCell(x,z,i++);
+            }
+        }
+    }
+
+    void Start(){
+        hexMesh.Triangulate(cells);
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            HandleInput();
+        }
+    }
+    void HandleInput(){
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(inputRay, out RaycastHit hit)){
+            TouchCell(hit.point);
+        }
+    }
+    void TouchCell(Vector3 position){
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * xWidht + coordinates.Z/2;
+        HexCell cell = cells[index];
+        cell.color = touchedColor;
+        hexMesh.Triangulate(cells);
+        print("touched at " + coordinates.ToString());
+    }
+
+    void CreateCell(int x, int z, int i){
+        Vector3 position;
+        position.x = (x + z * 0.5f - z/2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoords(x,z);
+        cell.color = defaultColor;
+
+        TMPro.TextMeshProUGUI label = Instantiate<TMPro.TextMeshProUGUI>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = 
+            new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+        label.transform.position += Vector3.up * .1f;
+    }
+}
