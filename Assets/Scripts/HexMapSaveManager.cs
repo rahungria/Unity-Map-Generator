1f3c16acdda8e1510784d6a285755e173dbc0067
Raphael Hungria <rhja@uol.com.br>
2019-11-19 17:15:31 -0300

    Replacing colors with textureArrays

--- Assets/Scripts/HexMapSaveManager.cs
+++ Assets/Scripts/HexMapSaveManager.cs
@@ -5,10 +5,12 @@ public static class HexMapSaveManager {
     private static HexGrid grid;
 
     public static void Save(HexGrid grid) {
-        string path = Path.Combine(Application.persistentDataPath, "test.hexsav");
+        DirectoryInfo info = Directory.CreateDirectory(Path.Combine(Application.persistentDataPath, "saves"));
+        string path = Path.Combine(info.FullName, "test.hexsav");
 
         using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create))) {
             //8 empty bytes for future proofing
+            Debug.Log(path);
             writer.Write((long)0);
             grid.Save(writer);
         }
